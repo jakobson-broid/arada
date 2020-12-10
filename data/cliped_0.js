b29880d29a90b9d5de2e2d0b593e7f4d53039e71
jakobson-broid <75787826+jakobson-broid@users.noreply.github.com>
2020-12-10 15:06:26 +0300

    Add files via upload

---
+++ data/cliped_0.js
@@ -0,0 +1,13 @@
+var json_cliped_0 = {
+"type": "FeatureCollection",
+"name": "cliped_0",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "4", "photo": null }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.777653048256447, 9.033502314383984 ] ] } },
+{ "type": "Feature", "properties": { "id": "6", "photo": "F:\\ZTE_Video\\Tezeta\\IMG_1073.jpg" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.753576168837846, 9.035856573148603 ] ] } },
+{ "type": "Feature", "properties": { "id": "8", "photo": "F:\\ZTE_Video\\Tezeta\\IMG_0914" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.739968785866452, 9.052755576339383 ] ] } },
+{ "type": "Feature", "properties": { "id": "9", "photo": "F:\\ZTE_Video\\Tezeta\\IMG_0930.jpg" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.74437987062857, 9.040582285383273 ] ] } },
+{ "type": "Feature", "properties": { "id": "10", "photo": "F:\\ZTE_Video\\Tezeta\\IMG_20151229_233037.jpg" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.761451883551494, 9.04031666601187 ] ] } },
+{ "type": "Feature", "properties": { "id": "12", "photo": "F:\\ZTE_Video\\Tezeta\\IMG_20160102_054054.jpg" }, "geometry": { "type": "MultiPoint", "coordinates": [ [ 38.767447673648078, 9.032228273238404 ] ] } }
+]
+}
